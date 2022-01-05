43f5fce16e75e95366b05778755330bdde6a4f94
sascha bajonczak <sba@realcore.de>
2022-01-05 20:13:17 +0100

    Completed demo

--- Program.cs
+++ Program.cs
@@ -30,20 +30,64 @@ ctx.SaveChanges();
 
 // Create second data entry
 City redmond = new City();
-redmond.CityName = "Redmond";
-redmond.Location = new Point(-122.123889, 47.669444) { SRID = 4326 };
+redmond.CityName = "Redmond (Center)";
+redmond.Location = new Point(-122.125569, 47.674222) { SRID = 4326 };
 ctx.Cities.Add(redmond);
 ctx.SaveChanges();
 
-// Create factory 
-var gf = NetTopologySuite.NtsGeometryServices.Instance.CreateGeometryFactory(4326);
 
+City poi1 = new City();
+poi1.CityName = "Poi 1 (15990 NE 85th St)";
+poi1.Location = new Point(-122.128070, 47.678498) { SRID = 4326 };
+ctx.Cities.Add(poi1);
+ctx.SaveChanges();
+
+City poi2 = new City();
+poi2.CityName = "Poi 2 (EvergreenHealth Medical Center Kirkland at Kirkland)";
+poi2.Location = new Point(-122.178200, 47.715305) { SRID = 4326 };
+ctx.Cities.Add(poi2);
+ctx.SaveChanges();
+
+
+// Create factory with SRID 4326
+var gf = NetTopologySuite.NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+
+// Current Locatin is "Remond Way" in Redmond
 // Set our Current Location
-var currentLocation = gf.CreatePoint(new NetTopologySuite.Geometries.Coordinate(-122.171936, 47.643159));
+var currentLocation = gf.CreatePoint(new NetTopologySuite.Geometries.Coordinate(-122.127812, 47.675470));
+
 
 // Find the nearest city in correlation to our location
-var nearestCity = ctx.Cities.OrderBy(c => c.Location.Distance(gf.CreateGeometry(currentLocation)));
+var nearestCity = ctx.Cities.OrderBy(c => c.Location.Distance(currentLocation));
 // Print out
-Console.WriteLine(String.Format("Nearest Citiy is : {0}", nearestCity.ToList().FirstOrDefault().CityName));
+Console.WriteLine(String.Format("Nearest City is : {0}", nearestCity.ToList().FirstOrDefault().CityName));
+Console.WriteLine(String.Format("======================"));
+Console.WriteLine(String.Format("Distance to targets (in miles)"));
+
+
+// Find the nearest city in correlation to our location
+var cities = ctx.Cities;
+
+foreach (var city in cities)
+{
+    var distanceInKM = city.Location.Distance(currentLocation) * 1000;// meters ;
+    Console.WriteLine(String.Format("Destination to : {0} is {1} ", city.CityName, distanceInKM));
+
+}
+
+Console.WriteLine(String.Format("======================"));
+Console.WriteLine(String.Format("Objects in 2 KM radius"));
+
+var distanceInMeters = 2 * 1609.344;
+
+var near2Kilometers = ctx.Cities.Where(_=> _.Location.IsWithinDistance(currentLocation, distanceInMeters));
+
+foreach (var city in near2Kilometers)
+{
+    var distanceInKM = city.Location.Distance(currentLocation) * 1000;
+    Console.WriteLine(String.Format("Found Poi's: {0} is {1}  {2}", city.CityName, distanceInKM, city.Location.AsText()));
+
+}
+
 
 Console.ReadLine();
